2b702f49cfd21e4599db0a9b24d28bbf86fdf2c4
João Eduardo Correa <joao.eduardu@gmail.com>
2019-11-27 12:50:02 -0300

    Commit com os migrations da base, bem como o script de criação das tabelas no SQL Server.

---
+++ TargetScheduler.DataAccess/Migrations/20191127154129_Init.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TargetScheduler.DataAccess.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "COM_Pessoas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: false),
+                    DataNascimento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_COM_Pessoas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "COM_Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PessoaId = table.Column<int>(nullable: false),
+                    Nome = table.Column<string>(nullable: true),
+                    Inicio = table.Column<DateTime>(nullable: false),
+                    Termino = table.Column<DateTime>(nullable: false),
+                    Tipo = table.Column<int>(nullable: false),
+                    Observacao = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_COM_Agendamentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_COM_Agendamentos_COM_Pessoas_PessoaId",
+                        column: x => x.PessoaId,
+                        principalTable: "COM_Pessoas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_COM_Agendamentos_PessoaId",
+                table: "COM_Agendamentos",
+                column: "PessoaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "COM_Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "COM_Pessoas");
+        }
+    }
+}
---
+++ scriptSQL.sql
@@ -0,0 +1,43 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [COM_Pessoas] (
+    [Id] int NOT NULL IDENTITY,
+    [Nome] nvarchar(max) NOT NULL,
+    [DataNascimento] datetime2 NOT NULL,
+    CONSTRAINT [PK_COM_Pessoas] PRIMARY KEY ([Id])
+);
+
+GO
+
+CREATE TABLE [COM_Agendamentos] (
+    [Id] int NOT NULL IDENTITY,
+    [PessoaId] int NOT NULL,
+    [Nome] nvarchar(max) NULL,
+    [Inicio] datetime2 NOT NULL,
+    [Termino] datetime2 NOT NULL,
+    [Tipo] int NOT NULL,
+    [Observacao] nvarchar(max) NULL,
+    CONSTRAINT [PK_COM_Agendamentos] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_COM_Agendamentos_COM_Pessoas_PessoaId] FOREIGN KEY ([PessoaId]) REFERENCES [COM_Pessoas] ([Id]) ON DELETE CASCADE
+);
+
+GO
+
+CREATE INDEX [IX_COM_Agendamentos_PessoaId] ON [COM_Agendamentos] ([PessoaId]);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20191127154129_Init', N'2.2.4-servicing-10062');
+
+GO
+
